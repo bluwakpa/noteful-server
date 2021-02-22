a1b016266e992b10f9545c6adfa172b2429615ea
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-02-22 15:42:56 -0800

    folder

--- seeds/seed.notes.sql
+++ seeds/seed.notes.sql
@@ -5,7 +5,7 @@ INSERT INTO notes (id, name, modified, folderId, content)
     ("d26e01a6-ffaf-11e8-8eb2-f2801f1b9fd1", "Pigs", "2018-03-01T00:00:00.000Z", "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1", faker.lorem.paragraphs()),
     ("d26e0570-ffaf-11e8-8eb2-f2801f1b9fd1", "Birds", "2019-01-04T00:00:00.000Z", "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1", faker.lorem.paragraphs()),
     ("d26e0714-ffaf-11e8-8eb2-f2801f1b9fd1", "Bears", "2018-07-12T23:00:00.000Z", "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1", faker.lorem.paragraphs()),
-    ("d26e0854-ffaf-11e8-8eb2-f2801f1b9fd1","Horses", "2018-08-20T23:00:00.000Z", "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1", faker.lorem.paragraphs()),
+    ("d26e0854-ffaf-11e8-8eb2-f2801f1b9fd1", "Horses", "2018-08-20T23:00:00.000Z", "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1", faker.lorem.paragraphs()),
     ("d26e0980-ffaf-11e8-8eb2-f2801f1b9fd1", "Tigers", "2018-03-03T00:00:00.000Z", "b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1", faker.lorem.paragraphs()),
     ("d26e0aac-ffaf-11e8-8eb2-f2801f1b9fd1", "Wolves", "2018-05-16T23:00:00.000Z", "b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1", faker.lorem.paragraphs()),
     ("d26e0bce-ffaf-11e8-8eb2-f2801f1b9fd1", "Elephants", "2018-04-11T23:00:00.000Z", "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1", faker.lorem.paragraphs()),
