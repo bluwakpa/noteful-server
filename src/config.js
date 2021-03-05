d3fdd76cf5e313a7a398fa48b952df3c2e0c6d33
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-03-05 14:37:06 -0800

    undo prod

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://ligrgkbzvzywgv:ba397808a2bedbd5e84cb575c0c79008e28844a57767d50d884d1fb2f89e4098@ec2-52-71-231-37.compute-1.amazonaws.com:5432/djsp12hb80hjk',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/noteful',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/noteful-test'
 }
